9c3aeea346cf5e7c22548698fc829f7cf320c70a
Giulio Abramo <giulio.abramo@gmail.com>
2022-04-06 12:13:52 +0200

    transformation operators

--- raytracer/raytracer/geometry2.cs
+++ raytracer/raytracer/geometry2.cs
@@ -4,8 +4,10 @@ using System.Reflection;
 using System.Transactions;
 using mialibreria;
 using static test.Test;
+using geometry;
+using Vector = geometry.Vector;
 
-namespace geometry;
+namespace geometry2;
 
 public struct Point
 {
@@ -51,23 +53,15 @@ public struct Point
     {
         return p * a;
     }
-/*
-    public Point Sum(Vector v)
+
+    public static Point operator +(Point p, Vector v)
     {
-        x += v.x;
-        y += v.y;
-        z += v.z;
-        return this;
+        return new Point(p.x + v.x, p.y + v.y, p.z + v.z);
     }
 
-    public Vector Diff(Point p)
+    public static Vector operator -(Point p,Vector v)
     {
-        Vector v = new Vector();
-        v.x = x - p.x;
-        v.y = y - p.y;
-        v.z = z - p.z;
-        
-        return v;
+        return new Vector(p.x - v.x, p.y - v.y, p.z - v.z);
     }
 
     public Vector toVector()
@@ -78,7 +72,7 @@ public struct Point
         v.z = z;
 
         return v;
-    }*/
+    }
 }
 
 public struct Transformation
@@ -96,7 +90,7 @@ public struct Transformation
         this.invm = invm;
         try
         {
-            Debug.Assert(m*invm == Matrix4x4.Identity);
+            Debug.Assert(IsClose(m*invm,Matrix4x4.Identity));
         }
         catch (Exception)
         {
@@ -105,8 +99,47 @@ public struct Transformation
         }
     }
 
-    public static Matrix4x4 operator *(Transformation A, Transformation B)
+    public static Transformation Traslation(Vector v)
     {
-        return Matrix4x4.Multiply(A.m,B.m);
+        var m = Matrix4x4.Identity + new Matrix4x4(0, 0, 0, v.x, 0, 0, 0, v.y, 0, 0, 0, v.z, 0, 0, 0, 0);
+        Matrix4x4 invm;
+        Matrix4x4.Invert(m, out invm);
+        return new Transformation(m,invm);
+    }
+
+    public Transformation Inverse()
+    {
+        return new Transformation(m:invm, invm:m);
+    }
+
+    public static Transformation operator *(Transformation A, Transformation B)
+    {
+        var m = A.m * B.m;
+        Matrix4x4 invm;
+        Matrix4x4.Invert(m, out invm);
+        
+        return new Transformation(m, invm);
+    }
+
+    public static Point operator *(Transformation A, Point p)
+    {
+        var mat = A.m;
+        var myPoint = new Point(
+            x: p.x*mat.M11*p.y*mat.M12*p.z*mat.M13+mat.M14,
+            y: p.x*mat.M21*p.y*mat.M22*p.z*mat.M23+mat.M24,
+            z: p.x*mat.M31*p.y*mat.M32*p.z*mat.M33+mat.M34);
+        var w = p.x * A.m.M41 * p.y * A.m.M42 * p.z * A.m.M43 + A.m.M44;
+        if (IsClose(w, 1.0f))
+            return myPoint;
+        return new Point(myPoint.x / w, myPoint.y / w, myPoint.z / w);
+    }
+
+    public static Normal operator *(Transformation A, Normal n)
+    {
+        var mat = A.invm;
+        return new Normal(
+            x: n.x*mat.M11*n.y*mat.M12*n.z*mat.M13+mat.M14,
+            y: n.x*mat.M21*n.y*mat.M22*n.z*mat.M23+mat.M24,
+            z: n.x*mat.M31*n.y*mat.M32*n.z*mat.M33+mat.M34);
     }
 }
