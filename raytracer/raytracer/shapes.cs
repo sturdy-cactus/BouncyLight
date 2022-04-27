1677bc45bab990c3ff5dd28f8cdeb7ea68a77170
matteo-martelli <matteo.martelli1@studenti.unimi.it>
2022-04-27 12:46:47 +0200

    x

--- raytracer/raytracer/shapes.cs
+++ raytracer/raytracer/shapes.cs
@@ -6,7 +6,7 @@ namespace shapes;
 
 interface IShape
 {
-    public HitRecord RayIntersection(Ray ray);
+    public HitRecord? RayIntersection(Ray ray);
 }
 
 class Sphere : IShape
@@ -21,16 +21,35 @@ class Sphere : IShape
     }
 
     //METHODS
-    public HitRecord RayIntersection(Ray ray)
+    public HitRecord? RayIntersection(Ray ray)
     {
+        //prep
+        float firstHit;
+        var hit = new HitRecord();
+        this.tr = this.tr.Inverse();
+        ray = this.tr * ray;
         var o = new Vector();
         o = ray.origin.ToVector();
+        
+        //body
         float dotprod = (float) Math.Pow(o * ray.direction, 2);
-
         float deltared = dotprod - ray.direction.SqNorm() * (o.SqNorm() - 1.0f);
+        if (deltared <= .0f)
+        {
+            return null;
+        }
+
+        float t1 = -(o * ray.direction + (float)Math.Sqrt(deltared)) / ray.direction.SqNorm();
+        float t2 = (-(o * ray.direction) + (float)Math.Sqrt(deltared)) / ray.direction.SqNorm();
+
+        if (t1 > ray.tMin && t1 < ray.tMax)
+            firstHit = t1;
+        else if (t2 > ray.tMin && t2 < ray.tMax)
+            firstHit = t2;
         
-        return HitRecord
         
+        
+
     }
 }
 
@@ -42,4 +61,14 @@ struct HitRecord
     public Vector2D surface_point;
     public float t;
     public Ray ray;
+    
+    //CONSTRUCTOR
+    public HitRecord(Point? world_point = null, Normal? n = null, Vector2D? surface_point = null, float? t = null, Ray? ray = null)
+    {
+        this.world_point = world_point ?? new Point();
+        this.n = n ?? new Normal();
+        this.surface_point = surface_point ?? new Vector2D();
+        this.t = t ?? .0f;
+        this.ray = ray ?? new Ray();
+    }
 }
