349bdd2abb26640a4d8cb9c1f19d03c6a1582545
matteo-martelli <matteo.martelli1@studenti.unimi.it>
2022-04-30 18:47:38 +0200

    xx

--- raytracer/raytracer/shapes.cs
+++ raytracer/raytracer/shapes.cs
@@ -55,11 +55,6 @@ class Sphere : IShape
         
         hit.t = firstHit;
         hit.ray = ray;
-
-        for (int i = 0; i < 4; i++)
-        {
-            Console.WriteLine("ok");
-        }
         
         return hit;
     }
