88125bfa840d57f507cd88acb72f1590567c8d69
Giulio Abramo <giulio.abramo@gmail.com>
2022-05-04 11:50:17 +0200

    begin demo

---
+++ raytracer/raytracer/demo.cs
@@ -0,0 +1,10 @@
+using Geometry;
+using PFMlib;
+using Cameras;
+using Shapes;
+
+var scala = Transformation.Scaling(new Vector(.1f, .1f, .1f));
+var sfera = new Sphere(scala);
+World mioMondo = new World();
+
+mioMondo.Add(sfera);
