ab0f92ff051329ec631ece4105d126d8fd52597e
Giulio Abramo <giulio.abramo@gmail.com>
2022-05-04 12:17:11 +0200

    spheres

--- raytracer/raytracer/demo.cs
+++ raytracer/raytracer/demo.cs
@@ -2,10 +2,43 @@ using Geometry;
 using PFMlib;
 using Cameras;
 using Shapes;
+using static Geometry.Transformation;
 
-var scala = Transformation.Scaling(new Vector(.1f, .1f, .1f));
-var sfera = new Sphere(scala);
+var scala = Scaling(new Vector(.1f, .1f, .1f));
 World mioMondo = new World();
 
 
-mioMondo.Add(sfera);
+var ctrasf = Translation(new Vector(-1, 0, 0));
+var cam = new PerspCamera(t:ctrasf);
+
+var p = Translation(new Vector(-.5f, -.5f, -.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(.5f,-.5f,-.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(-.5f,.5f,-.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(-.5f,-.5f,.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(.5f,.5f,.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(.5f,.5f,-.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(.5f,-.5f,.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(-.5f,.5f,.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+//sfere di controllo
+p=Translation(new Vector(0,0,-.5f));
+mioMondo.Add(new Sphere(scala*p));
+
+p=Translation(new Vector(0,.5f,0));
+mioMondo.Add(new Sphere(scala*p));
+
