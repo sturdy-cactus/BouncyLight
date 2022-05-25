4255d1aae5b68aa2f2124c10a2d40de39e8eb974
Giulio Abramo <giulio.abramo@gmail.com>
2022-05-25 13:00:42 +0200

    Token reading, beginning

--- raytracer/raytracer/scenefiles.cs
+++ raytracer/raytracer/scenefiles.cs
@@ -1,8 +1,82 @@
-﻿namespace scenefiles;
+﻿using System.Diagnostics;
 
-public struct Token
+namespace scenefiles;
+
+public class Token
 {
     public SourceLocation Location;
+    public string Member;
+    
+    public Token(string member, SourceLocation location)
+    {
+        Member = member;
+        Location = location;
+    }
+
+    public virtual string ToString()
+    {
+        return Member;
+    }
+}
+
+public class IdentifierToken : Token
+{
+    public IdentifierToken(string member, SourceLocation location) : base(member, location){}
+}
+
+public class KeywordToken : Token
+{
+    public KeywordToken(string member, SourceLocation location) : base(member, location){}
+}
+
+public class NumberToken : Token
+{
+    public float Float;
+
+    public NumberToken(string member, SourceLocation location) : base(member, location)
+    {
+        Float = Convert.ToSingle(member);
+    }
+}
+
+public class StopToken : Token
+{
+    public StopToken(string member, SourceLocation location) : base(member, location){}
+}
+
+public class StringToken : Token
+{
+    public StringToken(string member, SourceLocation location) : base(member, location){}
+}
+
+public class SymbolToken : Token
+{
+    public SymbolToken(string member, SourceLocation location) : base(member, location){}
+}
+
+
+
+public enum KeywordList
+{
+    NEW,
+    MATERIAL,
+    PLANE,
+    SPHERE,
+    DIFFUSE,
+    SPECULAR,
+    UNIFORM,
+    CHECKERED,
+    IMAGE,
+    IDENTITY,
+    TRANSLATION,
+    ROTATION_X,
+    ROTATION_Y,
+    ROTATION_Z,
+    SCALING,
+    CAMERA,
+    ORTHOGONAL,
+    PERSPECTIVE,
+    FLOAT
 }
 
 public struct InputStream
@@ -52,7 +126,7 @@ public struct InputStream
         }
     }
 
-    public char ReadCharacter()
+    public char ReadChar()
     {
         char character;
         
@@ -68,9 +142,63 @@ public struct InputStream
         }
 
         SavedLocation = Location;
-        UpdatePosition();
+        UpdatePosition(character);
+
+        return character;
     }
 
+    public void UnreadChar(char character)
+    {
+        Debug.Assert(SavedChar == '\0');
+        this.SavedChar = character;
+        this.Location = SavedLocation;
+    }
+
+    public void SkipWhitespace()
+    {
+        string Whitespace = " \n\r\t";
+
+        char character = (char)InputFile.Read();
+        while (Whitespace.Contains(character) || character == '#')
+        {
+            if (character == '#')
+                InputFile.ReadLine();
+            character = (char) InputFile.Read();
+            if (character.ToString() == "")
+                return;
+        }
+        UnreadChar(character);
+    }
+
+    public Token ReadToken()
+    {
+        SkipWhitespace();
+        string Symbols = "()[]<>,*";
+        
+        char character = (char) InputFile.Read();
+        
+        if (character.ToString() == "")
+            return new StopToken(character.ToString(),Location);
+
+        if (Symbols.Contains(character))
+            return new SymbolToken(character.ToString(), Location);
+
+        if (character == '"')
+        {
+            string myString = "";
+            while (character!='"')
+            {
+                myString += character;
+                character = (char) InputFile.Read();
+            }
+
+            return new StringToken(myString,Location);
+        }
+        
+        if (char.IsDigit(character)){}
+    }
+    
+    
 }
 
 public struct SourceLocation
